468290893f2ae16c46789782cac02fbbd1a6aabc
Gvenik <raffi_barseghyan@edu.aua.am>
2019-05-19 22:19:32 +0400

    Added user/username updtae

--- routes/users.js
+++ routes/users.js
@@ -31,6 +31,19 @@ router.delete('/:username', async (req, res, next)=> {
     }
 })
 
+router.put('/:username', async (req, res, next)=> {
+    try {
+        await userSchema.findOneAndUpdate({username: req.params.username}, {username: req.body.username})
+        res.status(200).send({
+            success: true,
+            message: `Username changed to ${req.body.username}`
+        })
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
 router.get('/', async (req, res)=> {
     const all = await userSchema.find({}, {_id: false, __v: false, password: false})
     res.status(200).send({
