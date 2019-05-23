df562a314064af65b37da0113ab45c4949969a24
Raffi Barseghyan <raffi.barseghyan@fluxtechs.com>
2019-05-23 19:42:08 +0400

    Improvement routing

--- routes/users.js
+++ routes/users.js
@@ -33,14 +33,30 @@ router.delete('/:username', async (req, res, next)=> {
 
 router.put('/:username', async (req, res, next)=> {
     try {
-        await userSchema.findOneAndUpdate({username: req.params.username}, {username: req.body.username})
+        await userSchema.findOneAndUpdate({username: req.params.username}, req.body)
         res.status(200).send({
             success: true,
-            message: `Username changed to ${req.body.username}`
+            message: `User updated`
         })
     }
     catch(err) {
         next(err)
+    }
+})
+
+router.get('/:username', async (req, res, next)=> {
+    const result = await userSchema.findOne({username: req.params.username}, {_id: false, __v: false, password: false})
+    if(result === null) {
+        res.status(400).send({
+            success: false,
+            message: `user with ${req.params.username} username not found`
+        })
+    }
+    else {
+        res.status(200).send({
+            success: true,
+            result: result
+        })
     }
 })
 
